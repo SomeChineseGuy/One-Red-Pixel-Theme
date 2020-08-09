90692440493da41b8c7645cd532260e9f8f712df
Alvin Ng <alvin.cl.ng@gmail.com>
2020-08-09 04:24:55 -0700

    Added header php and updated template

--- header.php
+++ header.php
@@ -4,6 +4,16 @@
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Montserrat|Roboto|Maven+Pro|Roboto+Mono&display=swap" rel="stylesheet">
+    <!-- Animate CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.2/animate.min.css">
+    <!-- jQuery -->
+    <script type="text/javascript"  src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <!-- Styles -->
     <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
--- main.php
+++ main.php
@@ -8,83 +8,50 @@
 */
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <title>One Red P!xel</title>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="shortcut icon" type="image/png" href="img/favicon.png"/>
-    <!-- Fonts -->
-    <link href="https://fonts.googleapis.com/css?family=Montserrat|Roboto|Maven+Pro|Roboto+Mono&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="fonts/stylesheet.css">
-    <!-- Font Awesome -->
-    <script src="https://kit.fontawesome.com/cd7e20452a.js"></script>
-    <!-- Animate CSS -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.2/animate.min.css">
-    <!-- jQuery -->
-    <script type="text/javascript"  src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <!-- Styles -->
-    
-    
-    
-</head>
-<body>
-    <section id="creative-section" class="wow fadeIn">
-        <h2>Always Stay Creative</h2>
-        <article>
-            <div class="left wow slideInLeft">
-                <p>Every creative journey starts with an idea. At One Red Pixel we believe that every creative journey starts with one pixel<span class="red">red.</span>
-                    That’s because we believe in a creative process that’s easy to start, delivers results and feels personal.
-                    <br>
-                    Our promise to you is that:
-                    <ul class="info">
-                        <li>We will communicate and be hands on in the creative process from start to finish</li>
-                        <li>We will work with you one-on-one to make thoughtful and professional design decisions</li>
-                        <li>As a small, dedicated team we will get your project finished on time and on budget - no middlemen.</li>
-                    </ul>
-                </p>
-            </div>
-            <div class="right wow slideInRight">
-                <div class="work-img"><div class="overlay"></div></div>
-            </div>
-        </article>
-
-        
-    </section>
-
-    <section id="demo-reo">
-
-    </section>
-
-    <section id="instagram" class="animated fadeIn">
-        
-    </section>
-    
-    <section id="client-project" class="row wow fadeIn">
-        <h2>Previous Clients</h2>
-          
-    </section>
-
-    <section id="about">
-        <h2>About</h2>
-        <div class="about-section">
-            <div class=" wow fadeInLeft">
-                <div class="info-image"></div>
-                <div class="red-background"></div>
-            </div>   
-            <p class="wow fadeInRight"><strong>Kris Schuman</strong> is just your friendly, neighbourhood Motion Graphics Artist and Filmmaker. Graduating with honours from Ai Vancouver with a focus on visual story telling and editing. There in Vancouver, and now based in Toronto, he has been honing his skills like a samurai, with great determination, passion and focus. In his downtime, when not working on a video, he's most likely to be found cooking in the kitchen, watching old episodes of Batman, or having a drink with friends. He is constantly striving to evolve his craft; challenging himself and in turn developing and strengthening his skills. Kris is a strong believer that there is always opportunity to grow, and that all dogs go to heaven. He enjoys and takes pride working with all his clients, big or small and from all walks of life. His skills and expertise are sought out by those searching for him to bring to life their visions. Whether it be personal projects or large video productions for major corporations such as, KPMG, TD BANK, and Trillium Health Partners, his genuine, passionate and creative personality is his guarantee.</p>
+<header class="main-header">
+    <video poster="img/desk-work.jpg" class="head-video" playsinline autoplay muted loop>
+        <source src="http://kris-site.local/wp-content/uploads/2020/08/SpaceRun.mp4" type="video/mp4">
+    </video>
+    <div class="content">
+        <h1>Every great idea starts with <span class="red-hover">One Pixel</span></h1>
+    </div>
+</header>
+<section id="creative-section" class="wow fadeIn">
+    <h2>Always Stay Creative</h2>
+    <article>
+        <div class="left wow slideInLeft">
+            <p>Every creative journey starts with an idea. At One Red Pixel we believe that every creative journey starts with one pixel<span class="red">red.</span>
+                That’s because we believe in a creative process that’s easy to start, delivers results and feels personal.
+                <br>
+                Our promise to you is that:
+                <ul class="info">
+                    <li>We will communicate and be hands on in the creative process from start to finish</li>
+                    <li>We will work with you one-on-one to make thoughtful and professional design decisions</li>
+                    <li>As a small, dedicated team we will get your project finished on time and on budget - no middlemen.</li>
+                </ul>
+            </p>
         </div>
-    </section>
-    <footer class="contact" id="contact">
+        <div class="right wow slideInRight">
+            <div class="work-img"><div class="overlay"></div></div>
+        </div>
+    </article>        
+</section>
 
-    </footer>
-    <script src="https://apps.elfsight.com/p/platform.js"></script>
-    <script src="js/stackedCards.min.js"></script>
-    <script src="js/main.js"></script>
-    <script src="js/wow.min.js"></script>
-    <script src="js/wave.js"></script>
-    
-</body>
-</html>
+<section id="client-project" class="row wow fadeIn">
+    <h2>Previous Clients</h2>
+        
+</section>
+
+<section id="about">
+    <h2>About</h2>
+    <div class="about-section">
+        <div class=" wow fadeInLeft">
+            <div class="info-image"></div>
+            <div class="red-background"></div>
+        </div>   
+        <p class="wow fadeInRight"><strong>Kris Schuman</strong> is just your friendly, neighbourhood Motion Graphics Artist and Filmmaker. Graduating with honours from Ai Vancouver with a focus on visual story telling and editing. There in Vancouver, and now based in Toronto, he has been honing his skills like a samurai, with great determination, passion and focus. In his downtime, when not working on a video, he's most likely to be found cooking in the kitchen, watching old episodes of Batman, or having a drink with friends. He is constantly striving to evolve his craft; challenging himself and in turn developing and strengthening his skills. Kris is a strong believer that there is always opportunity to grow, and that all dogs go to heaven. He enjoys and takes pride working with all his clients, big or small and from all walks of life. His skills and expertise are sought out by those searching for him to bring to life their visions. Whether it be personal projects or large video productions for major corporations such as, KPMG, TD BANK, and Trillium Health Partners, his genuine, passionate and creative personality is his guarantee.</p>
+    </div>
+</section>
+<footer class="contact" id="contact">
+    <?php echo do_shortcode('[contact-form-7 id="34" title="Contact form 1"]'); ?>
+</footer>
